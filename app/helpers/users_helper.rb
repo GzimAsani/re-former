d94c87aa625c2c7e9c9a4917b6e6871dc9c7e3dc
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-25 10:17:00 +0100

    add show.html and partials

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def user_params
-      params.require(:user).permit(:user_name, :email, :password)
+      params.require(:user).permit(:username, :email, :password)
   end     
 end
