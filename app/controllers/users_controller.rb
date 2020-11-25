1abc34654c202349990516f292ae908700fa3a8c
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-25 09:56:46 +0100

    add create action and flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,42 @@
 class UsersController < ApplicationController
-
-  def new
-    @user = User.new
+  include UsersHelper
+  before_action :set_user, only: %i[edit update show]
+  def show
+     
   end
 
-  def show
-    @user = User.find(params[:id]) 
+  def new
+   @user = User.new 
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-    flash[:success] = 'user was successfully created'
-    redirect_to @user
+      flash[:success] = 'user was successfully created'
+      redirect_to @user
     else
-    render 'new'
-    end 
+      render 'new'
+    end  
   end
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end 
+  def update 
+    if @user.update(user_params)
+      flash[:success] = 'user was sucessfully updated'
+      redirect_to @user
+    else
+      render 'new'          
+    end
+  end
 
+  def edit
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])    
+  end
 end
+
+
+
